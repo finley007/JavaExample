94f6576076bd5caa3b2c1babd3ff7089af233504
finley007 <finley916@gmail.com>
2014-12-02 04:07:36 -0500

    Update jaxb example

--- src/jaxb/JAXBExample.java
+++ src/jaxb/JAXBExample.java
@@ -11,20 +11,37 @@ import jaxb.Customer;
 public class JAXBExample {
 
 	public static void main(String[] args) {
+//		createCustomerXML();
+		createUserXML();
+		
+	}
+
+	private static void createUserXML() {
+		User user = new User();
+		byte id = 1;
+		user.setUId(id);
+		toXML(user);
+	}
+
+	private static void createCustomerXML() {
 		Customer customer = new Customer();
 		customer.setId(100);
 		customer.setName("mkyong");
 		customer.setAge(29);
+		toXML(customer);
+	}
+
+	private static void toXML(Object obj) {
 		try {
 			File file = new File("C:\\file.xml");
-			JAXBContext jaxbContext = JAXBContext.newInstance(Customer.class);
+			JAXBContext jaxbContext = JAXBContext.newInstance(obj.getClass());
 			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 
 			// output pretty printed
 			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
-			jaxbMarshaller.marshal(customer, file);
-			jaxbMarshaller.marshal(customer, System.out);
+			jaxbMarshaller.marshal(obj, file);
+			jaxbMarshaller.marshal(obj, System.out);
 		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
